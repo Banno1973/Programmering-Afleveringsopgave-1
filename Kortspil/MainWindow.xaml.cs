3dcf62373b23f17c5c15fccc1f7c5845c5027421
Per Nielsen <pernielsen8700@gmail.com>
2024-02-19 11:57:14 +0100

    Første udkast

--- Kortspil/MainWindow.xaml.cs
+++ Kortspil/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -28,13 +29,61 @@ namespace Kortspil
             
         }
 
+
         private string FindBillede(int kortnummer)
         {
-            string resultat = "2-Spar.jpg";
+            string kulør;
+            string kortnavn;
 
-            // Skriv din løsning her...
+            switch (kortnummer)
+            {
+                case (>= 1 and <= 13):
+                    kulør = "Spar";
+                    break;
+                case (>= 14 and <= 26):
+                    kulør = "Ruder";
+                    break;
+                case (>= 27 and <= 39):
+                    kulør = "Klør";
+                    break;
+                case (>= 40 and <= 52):
+                    kulør = "Hjerter";
+                    break;
+                default:
+                    kulør = "Ukendt";
+                    break;
+            }
+
+            switch (kortnummer)
+            {
+                case 1 or 14 or 27 or 40:
+                    kortnavn = "Es";
+                    break;
+                case 11 or 24 or 37 or 50:
+                    kortnavn = "Knægt";
+                    break;
+                case 12 or 25 or 38 or 51:
+                    kortnavn = "Dame";
+                    break;
+                case 13 or 26 or 39 or 51:
+                    kortnavn = "Konge";
+                    break;
+                default:
+                    kortnavn = kortnummer / Enum.Parse(Farve, kulør);
+                    break;
+            }
+
+            string resultat = $"{kortnavn}-{kulør}.jpg";
 
             return resultat;
         }
+
+        public enum Farve   
+        {
+            Spar = 1,
+            Ruder = 2,
+            Klør = 3,
+            Hjerter = 4
+        };
     }
 }
